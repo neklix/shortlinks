0b612b676a71f2adfd01a00bafe927890b6d7a57
t.chukanov <t001im27@gmail.com>
2025-04-01 20:29:58 +0300

    fix

--- src/config.py
+++ src/config.py
@@ -7,4 +7,7 @@ class Config:
         self.db_password = os.environ["POSTGRES_PASSWORD"]
         self.db_host = os.environ["POSTGRES_HOST"]
         self.db_port = os.environ["POSTGRES_PORT"]
+        self.db_connect_retries = 5
+        self.db_retry_delay_s = 10
+
 
